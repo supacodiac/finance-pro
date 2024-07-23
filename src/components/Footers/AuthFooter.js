03b4041e8c217456628d8223ca4c0c0793df7508
supacodiac <ptoshit1004@gmail.com>
2024-07-23 14:55:55 +0530

    Removed unnecessary clutters 1.0

--- src/components/Footers/AuthFooter.js
+++ src/components/Footers/AuthFooter.js
@@ -31,10 +31,11 @@ const Login = () => {
                 © {new Date().getFullYear()}{" "}
                 <a
                   className="font-weight-bold ml-1"
-                  href="https://www.creative-tim.com?ref=adr-auth-footer"
+                  href="https://www.sc.com/en/"
+                  rel="noopener noreferrer"
                   target="_blank"
                 >
-                  Creative Tim
+                  Standard Chartered
                 </a>
               </div>
             </Col>
@@ -42,36 +43,45 @@ const Login = () => {
               <Nav className="nav-footer justify-content-center justify-content-xl-end">
                 <NavItem>
                   <NavLink
-                    href="https://www.creative-tim.com?ref=adr-auth-footer"
+                    href="https://www.sc.com/en/"
+                    rel="noopener noreferrer"
                     target="_blank"
                   >
-                    Creative Tim
+                    Standard Chartered
                   </NavLink>
                 </NavItem>
+
                 <NavItem>
                   <NavLink
-                    href="https://www.creative-tim.com/presentation?ref=adr-auth-footer"
+                    href="https://www.sc.com/in/investment/sc-invest-mutual-fund-platform/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    SC Invest
+                  </NavLink>
+                </NavItem>
+
+                <NavItem>
+                  <NavLink
+                    href="https://www.sc.com/in/about-us/"
+                    rel="noopener noreferrer"
                     target="_blank"
                   >
                     About Us
                   </NavLink>
                 </NavItem>
-                <NavItem>
-                  <NavLink
-                    href="http://blog.creative-tim.com?ref=adr-auth-footer"
-                    target="_blank"
-                  >
-                    Blog
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md?ref=adr-auth-footer"
-                    target="_blank"
-                  >
-                    MIT License
-                  </NavLink>
-                </NavItem>
+
+                {/* <NavItem>
+              <NavLink
+              href="http://blog.creative-tim.com?ref=adr-admin-footer"
+              rel="noopener noreferrer"
+              target="_blank"
+              >
+              Blog
+              </NavLink>
+              </NavItem> */}
+
+
               </Nav>
             </Col>
           </Row>
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -228,13 +228,14 @@ const Sidebar = (props) => {
             </InputGroup>
           </Form>
           {/* Navigation */}
+
           <Nav navbar>{createLinks(routes)}</Nav>
           {/* Divider */}
           <hr className="my-3" />
           {/* Heading */}
-          <h6 className="navbar-heading text-muted">Documentation</h6>
+          {/* <h6 className="navbar-heading text-muted">Documentation</h6> */}
           {/* Navigation */}
-          <Nav className="mb-md-3" navbar>
+          {/* <Nav className="mb-md-3" navbar>
             <NavItem>
               <NavLink href="https://demos.creative-tim.com/argon-dashboard-react/#/documentation/overview?ref=adr-admin-sidebar">
                 <i className="ni ni-spaceship" />
@@ -253,15 +254,17 @@ const Sidebar = (props) => {
                 Components
               </NavLink>
             </NavItem>
-          </Nav>
-          <Nav className="mb-md-3" navbar>
+          </Nav> */}
+
+          {/* <Nav className="mb-md-3" navbar>
             <NavItem className="active-pro active">
               <NavLink href="https://www.creative-tim.com/product/argon-dashboard-pro-react?ref=adr-admin-sidebar">
                 <i className="ni ni-spaceship" />
                 Upgrade to PRO
               </NavLink>
             </NavItem>
-          </Nav>
+          </Nav> */}
+
         </Collapse>
       </Container>
     </Navbar>
